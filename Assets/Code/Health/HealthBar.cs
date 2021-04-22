bc1338aea1fc4d57021d80db674490a5e95fd02d
NiklasGranskog <granen.n@gmail.com>
2021-04-22 15:21:19 +0200

    Removed some usings in HealthBar

--- Assets/Code/Health/HealthBar.cs
+++ Assets/Code/Health/HealthBar.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,14 +6,8 @@ public class HealthBar : MonoBehaviour
     [SerializeField] private Image m_HealthBarImage;
     [SerializeField] private Gradient m_Gradient;
 
-    private void Awake()
-    {
-        m_HealthBarImage.color = m_Gradient.Evaluate(1f);
-    }
-
     private void Update()
     {
         m_HealthBarImage.color = m_Gradient.Evaluate(m_HealthBarImage.fillAmount);
     }
-
 }
