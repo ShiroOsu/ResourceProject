c9169bfe3240e0c1432288f62294f61afc9d5c8a
NiklasGranskog <granen.n@gmail.com>
2021-07-17 17:18:57 +0200

    'Custom Logger'
    I take no credit for this X)

--- Assets/Code/Framework/Enums/Enums.cs
+++ Assets/Code/Framework/Enums/Enums.cs
@@ -12,4 +12,20 @@ namespace Code.Framework.Enums
         Castle,
         Barracks,
     }
+    
+    public enum LogMaskThreshold
+    {
+        Undefined = -1, // 0xFFFFFFFF
+        Debuging = 0,
+        Normal = 1,
+        Critical = 2,
+        Nothing = 3,
+    }
+  
+    public enum LogMask
+    {
+        Debuging,
+        Normal,
+        Critical,
+    }
 }
