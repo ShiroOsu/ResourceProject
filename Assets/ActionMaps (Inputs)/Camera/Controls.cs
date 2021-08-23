49071cd654bd4710651ec36193fcc32fd709b775
NiklasGranskog <granen.n@gmail.com>
2021-08-23 19:26:38 +0200

    Missing namespace in actionmap

--- Assets/ActionMaps (Inputs)/Camera/Controls.cs
+++ Assets/ActionMaps (Inputs)/Camera/Controls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Inputs/Camera/Controls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/ActionMaps (Inputs)/Camera/Controls.inputactions'
 
 using System;
 using System.Collections;
