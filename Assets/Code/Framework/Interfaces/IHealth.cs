ed8fca1bdd8ba4e50b8b64d5c17687f33a231100
NiklasGranskog <granen.n@gmail.com>
2021-08-24 19:12:44 +0200

    test

--- Assets/Code/Framework/Interfaces/IHealth.cs
+++
@@ -1,8 +0,0 @@
-namespace Code.Framework.Interfaces
-{
-    public interface IHealth
-    {
-        public void ModifyHealth(float amount);
-        public void RegenHealth(/*float amount*/);
-    }
-}
--- Assets/Code/Managers/GameManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Code.Managers
-{
-    public class GameManager : MonoBehaviour
-    {
-        // Hello
-    }
-}
